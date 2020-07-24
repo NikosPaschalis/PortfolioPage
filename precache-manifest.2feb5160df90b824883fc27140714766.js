01ec6ccba8a09674cf5c4d0d5cdd0329f0866ba5
Νικόλαος Πασχάλης <n.pashalis@DYPE.LOCAL>
2020-07-24 09:14:18 +0300

    Updates

---
+++ precache-manifest.2feb5160df90b824883fc27140714766.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cac97f77ef6564576465def31a945d9",
+    "url": "/PortfolioPage/index.html"
+  },
+  {
+    "revision": "5ed663036c7a36421a36",
+    "url": "/PortfolioPage/static/css/main.78df8b74.chunk.css"
+  },
+  {
+    "revision": "3c00d75160ef4005eabc",
+    "url": "/PortfolioPage/static/js/2.b094fffc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/PortfolioPage/static/js/2.b094fffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ed663036c7a36421a36",
+    "url": "/PortfolioPage/static/js/main.2cf80cde.chunk.js"
+  },
+  {
+    "revision": "d9d1057bc4065e3d8697",
+    "url": "/PortfolioPage/static/js/runtime-main.728224cb.js"
+  }
+]);
