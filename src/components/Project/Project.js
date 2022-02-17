cdcf9292136db715dd85621a3e346e3e9544b2b3
Nikos Paschalis <nikospasxalis94@gmail.com>
2022-02-17 12:22:31 +0200

    minor css changes for readability

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 const Project = (props) => (
   
-                <Card style={{ width: '18rem'}}>
+                <Card style={{ width: '18rem' }}>
         <Card.Body style = {{backgroundColor: '#164D56'}}>
         <Card.Title>{props.name}</Card.Title>
             <Card.Text>
